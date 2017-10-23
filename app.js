7e33f813b7b8144d85af3faab52de26dac9224dc
Bec <bec.braughton@gmail.com>
2017-10-23 02:23:43 -0500

    add attendee

--- app.js
+++ app.js
@@ -40,3 +40,40 @@ $(document).ready(function () {
         addAttendee();
         syncPurchaseButton();
     });
+
+
+    function addAttendee() {
+        $('.attendee-list').append(
+            $('script[data-template="attendee"]').text()
+        );
+    
+        // Sync remove button UI
+        syncRemoveButtons();
+    }
+    
+    function syncRemoveButtons() {
+        // If only one attendee, hide the first remove button
+        // otherwise, show all remove buttons
+        if (getAttendeeCount() === 1) {
+            $('.attendee-list .attendee .remove-attendee').first().hide();
+        } else {
+            $('.attendee-list .attendee .remove-attendee').show();
+        }
+    }
+    
+    function syncPurchaseButton() {
+        // Total up the count for the checkout button total
+        $('#checkout-button span.amount').html(
+            '$' + data.cost * getAttendeeCount()
+        );
+    }
+    
+    // Events
+    $('.add-attendee').on('click', function (event) {
+        event.preventDefault();
+        addAttendee();
+        $(this).trigger('attendee:add');
+    }).on('attendee:add', function () {
+        syncPurchaseButton();
+        syncRemoveButtons();
+    });
