5091a21b06f34885e7b6bc8b264e38b7d7dce2d2
Bec <bec.braughton@gmail.com>
2017-10-23 02:17:36 -0500

    sync purchase button

---
+++ app.js
@@ -0,0 +1,42 @@
+// Author: Bec Braughton
+// Filename: JQTutorial.html
+// Date: 10.23.27
+// CIS 131
+
+$(document).ready(function () {
+    
+        var data = {
+            cost: 9.99
+        };
+    
+        /**
+         * Get the attendee count
+         */
+        function getAttendeeCount() {
+            return $('.attendee-list .row.attendee').length;
+        }
+    
+        function addAttendee() {
+            $('.attendee-list').append(
+                $('script[data-template="attendee"]').text()
+            );
+        }
+    
+        function syncPurchaseButton() {
+            // Total up the count for the checkout button total
+            $('#checkout-button span.amount').html(
+                '$' + data.cost * getAttendeeCount()
+            );
+        }
+    
+        //
+        // Initialize the form
+        //
+    
+        // Set up the unit cost of one ticket
+        $('#unit-price').html('$' + data.cost + ' ea.');
+    
+        // Add one attendee by default on init
+        addAttendee();
+        syncPurchaseButton();
+    });
